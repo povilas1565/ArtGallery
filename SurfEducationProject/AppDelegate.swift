aac4d8a432c79875f50ff40a3de815aa956a5a05
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-09 16:45:20 +0300

    Add login logic and rerequest token if needed

--- SurfEducationProject/AppDelegate.swift
+++ SurfEducationProject/AppDelegate.swift
@@ -6,6 +6,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var tokenStorage: TokenStorage {
+        BaseTokenStorage()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
@@ -14,13 +16,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             window?.overrideUserInterfaceStyle = .light
         }
 
+        startApplicationProccess()
+
+        return true
+        }
+
         runMainFlow()
 
         return true
     }
 
+    func startApplicationProccess() {
+        runLaunchScreen()
+
+        if let tokenContainer = try? tokenStorage.getToken(), !tokenContainer.isExpired {
+        } else {
+            let tempCredentials = AuthRequestModel(phone: "+79876543219", password: "qwerty")
+            AuthService()
+                    .performLoginRequestAndSaveToken(credentials: tempCredentials) { [weak self] result in
+                        switch result {
+                        case .success:
+                            self?.runMainFlow()
+                        case .failure:
+                            // TODO: - Handle error, if token was not received
+                            break
+                        }
+                    }
+        }
+    }
+
     func runMainFlow() {
-        window?.rootViewController = TabBarConfigurator().configure()
+        DispatchQueue.main.async {
+            self.window?.rootViewController = TabBarConfigurator().configure()
+        }
+    }
+
+    func runLaunchScreen() {
+        let lauchScreenViewController = UIStoryboard(name: "LaunchScreen", bundle: .main)
+                .instantiateInitialViewController()
+
+        window?.rootViewController = lauchScreenViewController
     }
 
 }
