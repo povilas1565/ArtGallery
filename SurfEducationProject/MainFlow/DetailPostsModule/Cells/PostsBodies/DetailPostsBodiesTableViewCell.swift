8f2ef8133a4f0d83b77174a87e2b670f2f9b71b9
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-11 21:48:58 +0300

    Закончена работа на экране информации о посте.

---
+++ SurfEducationProject/MainFlow/DetailPostsModule/Cells/PostsBodies/DetailPostsBodiesTableViewCell.swift
@@ -0,0 +1,34 @@
+
+//
+//  DetailPostsBodiesTableViewCell.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Павел Рыжков on 07.08.2022.
+//
+import UIKit
+
+class DetailPostsBodiesTableViewCell: UITableViewCell {
+
+    //MARK: - Views
+    @IBOutlet weak var postsBodiesText: UILabel!
+
+    //MARK: - Properties
+    var bodiesText: String = "" {
+        didSet {
+            postBodiesText.text = bodiesText
+        }
+    }
+
+    //MARK: - Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureApperance()
+    }
+
+    private func configureAppearance() {
+        selectionStyle = .none
+        postBodiesText.font = .systemFont(ofSize: 15, weight: .light)
+        postBodiesText.textColor = .black
+    }
+
+}
