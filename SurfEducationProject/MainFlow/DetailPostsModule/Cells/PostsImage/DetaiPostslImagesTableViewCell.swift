a43ebbe4656bcf7aa5a2156484abb1dedffbbb01
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-11 22:03:29 +0300

    Добавлен UIAlertController.

---
+++ SurfEducationProject/MainFlow/DetailPostsModule/Cells/PostsImage/DetaiPostslImagesTableViewCell.swift
@@ -0,0 +1,60 @@
+
+//
+//  DetailPostsImagesTableViewCell.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Павел Рыжков on 07.08.2022.
+//
+import UIKit
+
+class DetailedPostsImagesTableViewCell: UITableViewCell {
+
+    //MARK: - Constants
+    private enum Constants {
+        static let favoriteTapped = UIImage(named: "favoriteTapped")
+        static let favoriteUntapped = UIImage(named: "favoriteUntapped")
+    }
+    let favoritesStorage = FavoritesStorage.shared
+    var postTextLabel: String = ""
+
+    //MARK: - Views
+
+    @IBOutlet private weak var detailedPostsImagesViews: UIImagesViews!
+    @IBOutlet weak var favoriteButtonLabel: UIButton!
+
+
+    //MARK: - Events // Реализуется позже
+
+    var didFavoriteTap: (() -> Void)?
+    @IBAction func favoriteButtonAction(_ sender: Any) {
+        didFavoriteTap?()
+    }
+
+    //MARK: - Calculated
+    var buttonImage: UIImage? {
+        return isFavorite ? Constants.favoriteTapped : Constants.favoriteUntapped
+    }
+
+    //MARK: - Properties
+    var imageUrlInString: String = "" {
+        didSet {
+            guard let url = URL(string: imageUrlInString) else {
+                return
+            }
+            detailedPostImageView?.loadImage(from: url)
+        }
+    }
+    var isFavorite = false {
+        didSet {
+            favoriteButtonLabel.setImage(buttonImage, for: .normal)
+        }
+    }
+
+    //MARK: - Methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        selectionStyle = .none
+        detailPostsImagesViews.layer.cornerRadius = 15
+        detailPostsImagesViews.contentMode = .scaleAspectFill
+    }
+}
