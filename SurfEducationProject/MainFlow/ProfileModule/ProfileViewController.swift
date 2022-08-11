8fe0b4dfb9a6179b7aefafc4e418e3cae38c93a1
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-11 20:21:36 +0300

    Добавлена верстка главного экрана (данные всё еще на моках).

--- SurfEducationProject/MainFlow/ProfileModule/ProfileViewController.swift
+++ SurfEducationProject/MainFlow/ProfileModule/ProfileViewController.swift
@@ -1,4 +1,9 @@
-
+//
+//  ProfileViewController.swift
+//  SurfSummerSchoolProject
+//
+//  Created by Рыжков Павел on 04.08.2022.
+//
 import UIKit
 
 final class ProfileViewController: UIViewController {
