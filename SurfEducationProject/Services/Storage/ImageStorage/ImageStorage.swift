1289e6b69ce442b622f7e4ba06f379ea51fc3cc8
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-15 20:27:14 +0300

    Добавлена кнопка показа/сокрытия пароля.

---
+++ SurfEducationProject/Services/Storage/ImageStorage/ImageStorage.swift
@@ -0,0 +1,16 @@
+//
+//  ImageStorage.swift
+//  SurfSummerSchoolProject
+//
+// Created by Павел Рыжков on 15.08.2022.
+//
+
+import Foundation
+import UIKit
+
+struct ImageStorage {
+    static let passwordIsShownIcon = UIImage(named: "passwordShown")
+    static let passwordIsHiddenIcon = UIImage(named: "passwordHidden")
+
+    }
+
