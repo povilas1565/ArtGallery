388ee589f585eab0647d0c50ee158e835e4030da
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-17 22:36:59 +0300

    Данные на вкладке Профиля загружаются из интернета

--- SurfEducationProject/Services/Networking/AuthService/AuthService.swift
+++ SurfEducationProject/Services/Networking/AuthService/AuthService.swift
@@ -15,19 +15,20 @@ struct AuthService {
         path: "auth/login"
     )
 
-    func performLoginRequest(
-        credentials: AuthRequestModel,
-        _ onResponseWasReceived: @escaping (_ result: Result<AuthResponseModel, Error>) -> Void
+    func performLoginRequestAndSaveToken(
+            credentials: AuthRequestModel,
+            _ onResponseWasReceived: @escaping (_ result: Result<AuthResponseModel, Error>) -> Void
     ) {
         dataTask.performRequest(input: credentials) { result in
             if case let .success(responseModel) = result {
                 do {
                     try dataTask.tokenStorage.set(newToken: TokenContainer(token: responseModel.token, receivingDate: .now))
+                    try dataTask.profileStorage.set(profile: responseModel.user_info)
                 } catch {
+
                     // TODO: - Handle error if token not was received from server
                 }
             }
-
             onResponseWasReceived(result)
         }
     }
