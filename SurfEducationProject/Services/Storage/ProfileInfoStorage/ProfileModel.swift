c9729021f374093025ec42e19968b1f1eb421cfd
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-17 22:29:43 +0300

    Добавлена модель для Профиля, данные загружаются из интернета

---
+++ SurfEducationProject/Services/Storage/ProfileInfoStorage/ProfileModel.swift
@@ -0,0 +1,34 @@
+//
+//  BaseProfileStorage.swift
+//  SurfSummerSchoolProject
+//
+// Created by Павел Рыжков on 17.08.2022.
+//
+
+import Foundation
+
+struct ProfileModel: Decodable {
+    let phone: String
+    let email: String
+    let firstName: String
+    let lastName: String
+    let avatar: String
+    let city: String
+    let about: String
+}
+
+struct ProfileInstance {
+    static let shared = ProfileInstance()
+    let profileModel: ProfileModel
+
+    init() {
+        let storage = BaseProfileStorage()
+        do {
+            let profile = try storage.getProfileInfo()
+            self.profileModel = profile
+        } catch {
+            print(error)
+            self.profileModel = ProfileModel(phone: "+7 (9**) *** ** **", email: "alexandra@surfstudio.ru", firstName: "Александра", lastName: "Новикова", avatar: "", city: "Санкт-Петербург", about: "Что-то не так с загрузкой профиля :)")
+        }
+    }
+}
