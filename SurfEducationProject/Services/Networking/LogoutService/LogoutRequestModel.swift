b136b6ce5ec0190a6956c1faa4fd793fa7017a3f
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-08-17 22:40:59 +0300

    Добавлена реализация сервиса Logout

---
+++ SurfEducationProject/Services/Networking/LogoutService/LogoutRequestModel.swift
@@ -0,0 +1,13 @@
+//
+//  LogoutRequestModel.swift
+//  SurfSummerSchoolProject
+//
+// Created by Павел Рыжков on 17.08.2022.
+//
+
+import Foundation
+
+struct LogoutRequestModel: Encodable {
+
+}
+
